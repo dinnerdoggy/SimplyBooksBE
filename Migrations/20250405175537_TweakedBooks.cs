d6321e7c7157c02e8d7321f2eaff9bbdfb5e4c44
Casey <dinnerdoggy@gmail.com>
2025-04-05 13:02:49 -0500

    refacotred reads

---
+++ Migrations/20250405175537_TweakedBooks.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimplyBooksBE.Migrations
+{
+    /// <inheritdoc />
+    public partial class TweakedBooks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Description", "Price" },
+                values: new object[] { "It's just an okay book", 99.99m });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Description", "Price" },
+                values: new object[] { "The Best Book Ever", 49.99m });
+        }
+    }
+}
